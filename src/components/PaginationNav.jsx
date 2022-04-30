d342989cb54c9180ad5214fb1aba5e2f9acfe1c4
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-30 20:47:02 +0530

    feat - pagination complete

--- src/components/PaginationNav.jsx
+++ src/components/PaginationNav.jsx
@@ -1,4 +1,10 @@
 import { usePage } from "../contexts/page.context";
+import styles from '../App.module.css'
+
+let pgNumbers = []
+for (let i = 1; i <= 42; i++) {
+    pgNumbers[i] = i
+}
 
 const PaginationNav = () => {
     const { pgNumber, setPgNumber } = usePage()
@@ -11,31 +17,36 @@ const PaginationNav = () => {
         setPgNumber(p => p + 1)
     }
 
+    const handlePgNumClick = (e) => {
+        setPgNumber(Number(e.target.value))
+    }
+
     return (
-        <nav className="flx flx-center mg-top-md mg-btm-md">
-            {
-                pgNumber > 1 &&
-                <button onClick={handlePrevClick} className="btn-outlined b-solid b-primary txt-md txt-primary pd-xs mg-right-xs">
-                    prev
-                </button>
-            }
-            <button className="btn-solid txt-md txt-secondary bg-secondary pd-xs mg-right-xs">
-                1
-            </button>
-            <button className="btn-txt txt-md txt-primary pd-xs mg-right-xs">
-                2
-            </button>
-            <button className="btn-txt txt-md txt-primary pd-s mg-right-xs">
-                3
-            </button>
-            <p className="txt-primary txt-md mg-right-xs">....</p>
-            {
-                pgNumber < 42 &&
-                <button onClick={handleNextClick} className="btn-outlined b-solid b-primary txt-md txt-primary pd-xs mg-right-xs">
-                    next
-                </button>
-            }
-        </nav>
+        <div className="flx flx-center">
+            <nav className={`${styles.pgNav} flx flx-center mg-top-md mg-btm-md`}>
+                {
+                    pgNumber > 1 &&
+                    <button onClick={handlePrevClick} className="btn-outlined b-solid b-primary txt-md txt-primary pd-xs mg-right-xs">
+                        prev
+                    </button>
+                }
+                {
+                    pgNumbers.map(num => (num > pgNumber + 5 || num < pgNumber - 5) ? '' : <button key={num} value={num} onClick={handlePgNumClick} className={`${num === pgNumber ? 'btn-solid bg-secondary txt-secondary' : 'btn-outlined b-solid b-primary txt-primary bg-primary'}  txt-md pd-xs mg-right-xs`}>
+                        {num}
+                    </button>)
+                }
+                {
+                    pgNumber < 42 &&
+                    <p className="txt-primary txt-md mg-right-xs">....</p>
+                }
+                {
+                    pgNumber < 42 &&
+                    <button onClick={handleNextClick} className="btn-outlined b-solid b-primary txt-md txt-primary pd-xs">
+                        next
+                    </button>
+                }
+            </nav>
+        </div>
     );
 };
 
