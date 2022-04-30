f10d9ec39361fbe5ba3ba1ecfe1a74211c25dbde
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-30 17:41:41 +0530

    feat - get characters API and ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import CharacterCard from "./components/CharacterCard";
 import Header from "./components/Header";
 import PaginationNav from "./components/PaginationNav";
+import styles from './App.module.css'
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("https://rickandmortyapi.com/api/character?page=1")
+      setCharacters(response.data.results)
+    })()
+  }, [])
 
   return (
     <div
@@ -13,8 +23,10 @@ const App = () => {
     >
       <Header />
       <PaginationNav />
-      <section className="grid grid-maxcols-4 mg-s">
-        <CharacterCard />
+      <section className={`grid grid-maxcols-4 ${styles.cardsGrid} mg-s`}>
+        {
+          characters?.map(char => <CharacterCard key={char.id} character={char} />)
+        }
       </section>
     </div>
   );
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,29 +1,29 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import styles from '../App.module.css'
 
-const CharacterCard = () => {
-    const [character, setCharacter] = useState({});
+const CharacterCard = ({ character }) => {
     const [origin, setOrigin] = useState({});
     const [location, setLocation] = useState({});
     const [episodes, setEpisodes] = useState([]);
 
     useEffect(() => {
         (async () => {
-            const response = await axios.get("https://rickandmortyapi.com/api/character/394");
-            setCharacter(response.data);
+            if (Object.keys(character).length > 0) {
+                const response = await axios.get(character.origin.url);
+                setOrigin(response.data);
+            }
         })();
-    }, []);
+    }, [character]);
 
     useEffect(() => {
         (async () => {
             if (Object.keys(character).length > 0) {
-                const response = await axios.get(`${character.origin.url}`);
-                setOrigin(response.data);
-                const response2 = await axios.get(`${character.location.url}`);
-                setLocation(response2.data);
+                const response = await axios.get(character.location.url);
+                setLocation(response.data);
             }
         })();
-    }, [character]);
+    }, [character])
 
     useEffect(() => {
         (async () => {
@@ -38,37 +38,34 @@ const CharacterCard = () => {
     }, [character]);
 
     return (
-        <article className="card-dim card-shadow-xs pd-xs">
+        <article className={` ${styles.characterCard} card-shadow-xs pd-xs`}>
             <div className="pos-relative">
-                <img srcSet={character?.image} alt={character?.name} />
+                <img srcSet={character?.image} alt={character?.name} className={styles.charImage} />
                 <p className="txt-md txt-500 bg-primary txt-primary txt-cap pos-absolute tr-1 pd-xs">{character?.name}</p>
             </div>
-            <div className="flx flx-min-center mg-top-xs mg-left-xs">
-                <div className={`badge-size-xs ${character?.status === "Alive" ? "bg-success" : "bg-err"}  brd-full`}>
+            <div className="flx flx-min-center mg-top-xs mg-left-xs mg-btm-s">
+                <div className={`badge-size-xs ${character?.status === "Alive" ? "bg-success" : character?.status === "Dead" ? "bg-err" : "bg-off-primary"}  brd-full`}>
                 </div>
                 <p className="txt-md txt-primary txt-cap mg-left-xs">{character?.status}</p>
                 <p className="txt-md txt-primary txt-cap mg-left-xs">{`- ${character?.species}`}</p>
             </div>
             <div className="flx flx-maj-stretch mg-top-xs">
                 <div className="flx flx-column">
-                    <p className="txt-md txt-300 txt-off-primary txt-cap">last location</p>
-                    <p className="txt-md txt-primary txt-cap">{character?.location?.name}{" "}{location?.residents?.length}</p>
+                    <p className="txt-md txt-300 txt-off-primary txt-cap mg-btm-xs">last location</p>
+                    <p className="txt-md txt-primary txt-cap">{character?.location?.name}</p>
+                    <p className="txt-md txt-primary txt-cap">{location?.residents?.length} residents</p>
                 </div>
                 <div className="flx flx-column">
-                    <p className="txt-md txt-300 txt-off-primary txt-cap">origins</p>
-                    <p className="txt-md txt-primary txt-cap">{character?.origin?.name}{" "}{origin?.residents?.length}</p>
+                    <p className="txt-md txt-300 txt-off-primary txt-cap mg-btm-xs">origins</p>
+                    <p className="txt-md txt-primary txt-cap">{character?.origin?.name}</p>
+                    <p className="txt-md txt-primary txt-cap">{origin?.residents?.length} residents</p>
                 </div>
             </div>
             <div className="flx flx-column mg-top-s">
-                <p className="txt-md txt-300 txt-primary txt-cap">
-                    episodes
+                <p className="txt-md txt-300 txt-off-primary txt-cap mg-btm-xs">
+                    {`${character.name}'s first episode`}
                 </p>
-                <ul className="flx">
-                    {
-                        episodes?.map(ep =>
-                            <li key={ep} className="txt-primary txt-md pd-xs mg-right-xs">{ep}</li>)
-                    }
-                </ul>
+                <p className="txt-md txt-primary txt-cap">{episodes[0]}</p>
             </div>
         </article>
     );
